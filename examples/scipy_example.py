c6fe56e4d5309ddc5537181fc32fe7cbdef4cd5a
Andrew Tarzia <andrew.tarzia@gmail.com>
2021-06-01 17:21:40 +0100

    Use a cage example.

--- examples/scipy_example.py
+++ examples/scipy_example.py
@@ -1,46 +1,103 @@
+from collections import defaultdict
 import stk
 import mchammer as mch
 
 
-triangle = stk.BuildingBlock.init_from_file('triangle.mol')
-triangle_atoms = [
-    (atom.get_id(), atom.__class__.__name__)
-    for atom in triangle.get_atoms()
-]
-triangle_bonds = []
-for i, bond in enumerate(triangle.get_bonds()):
-    b_ids = (bond.get_atom1().get_id(), bond.get_atom2().get_id())
-    triangle_bonds.append((i, b_ids))
+def get_long_bond_ids(mol):
+    """
+    Find long bonds in stk.ConstructedMolecule.
 
+    """
+    long_bond_ids = []
+    for bond_infos in mol.get_bond_infos():
+        if bond_infos.get_building_block() is None:
+            ids = (
+                bond_infos.get_bond().get_atom1().get_id(),
+                bond_infos.get_bond().get_atom2().get_id(),
+            )
+            long_bond_ids.append(ids)
+
+    return tuple(long_bond_ids)
+
+
+def get_subunits(mol):
+    """
+    Get connected graphs based on building block ids.
+
+    Returns
+    -------
+    subunits : :class:`.dict`
+        The subunits of `mol` split by building block id. Key is
+        subunit identifier, Value is :class:`iterable` of atom ids in
+        subunit.
+
+    """
+
+    subunits = defaultdict(list)
+    for atom_info in mol.get_atom_infos():
+        subunits[atom_info.get_building_block_id()].append(
+            atom_info.get_atom().get_id()
+        )
+
+    return subunits
+
+
+# Building a cage from the examples on the stk docs.
+bb1 = stk.BuildingBlock(
+    smiles='O=CC(C=O)C=O',
+    functional_groups=[stk.AldehydeFactory()],
+)
+bb2 = stk.BuildingBlock(
+    smiles='O=CC(Cl)(C=O)C=O',
+    functional_groups=[stk.AldehydeFactory()],
+)
+bb3 = stk.BuildingBlock('NCCN', [stk.PrimaryAminoFactory()])
+bb5 = stk.BuildingBlock('NCCCCN', [stk.PrimaryAminoFactory()])
+
+cage = stk.ConstructedMolecule(
+    topology_graph=stk.cage.FourPlusSix(
+        # building_blocks is now a dict, which maps building
+        # blocks to the id of the vertices it should be placed
+        # on. You can use ranges to specify the ids.
+        building_blocks={
+            bb1: range(2),
+            bb2: (2, 3),
+            bb3: 4,
+            bb5: range(5, 10),
+        },
+    ),
+)
+cage.write('poc2.mol')
+stk_long_bond_ids = get_long_bond_ids(cage)
 mch_mol = mch.Molecule(
     atoms=(
-        mch.Atom(id=i[0], element_string=i[1]) for i in triangle_atoms
+        mch.Atom(
+            id=atom.get_id(),
+            element_string=atom.__class__.__name__,
+        ) for atom in cage.get_atoms()
     ),
     bonds=(
-        mch.Bond(id=i[0], atom_ids=i[1])
-        for i in triangle_bonds
+        mch.Bond(
+            id=i,
+            atom_ids=(
+                bond.get_atom1().get_id(),
+                bond.get_atom2().get_id(),
+            )
+        ) for i, bond in enumerate(cage.get_bonds())
     ),
-    position_matrix=triangle.get_position_matrix(),
+    position_matrix=cage.get_position_matrix(),
 )
 
 target_bond_length = 1.2
-bond_pair_ids = ((6, 32), (11, 12), (16, 33))
 optimizer = mch.ScipyOptimizer(
-    step_size=0.25,
     target_bond_length=target_bond_length,
     num_steps=100,
 )
-subunits = mch_mol.get_subunits(
-    bond_pair_ids=bond_pair_ids,
-)
-# Get all steps.
-mch_mol, mch_result = optimizer.get_trajectory(
-    mol=mch_mol,
-    bond_pair_ids=bond_pair_ids,
-    subunits=subunits,
-)
-triangle = triangle.with_position_matrix(mch_mol.get_position_matrix())
-triangle.write('triangle_opt.mol')
 
-with open('triangle_opt.out', 'w') as f:
-    f.write(mch_result.get_log())
+# Just get final step.
+mch_mol, mch_result = optimizer.get_result(
+    mol=mch_mol,
+    bond_pair_ids=stk_long_bond_ids,
+)
+cage = cage.with_position_matrix(mch_mol.get_position_matrix())
+cage.write('poc2_scipy_opt.mol')
