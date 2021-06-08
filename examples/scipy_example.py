6e989f7e5ad771d711cb6ba5c02b746dc677fbc8
Andrew Tarzia <andrew.tarzia@gmail.com>
2021-06-08 17:30:19 +0100

    WIP: example.

--- examples/scipy_example.py
+++ examples/scipy_example.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 import stk
 import mchammer as mch
+from copy import deepcopy
 
 
 def get_long_bond_ids(mol):
@@ -58,11 +59,8 @@ bb1 = stk.BuildingBlock(
         deleters=(),
     )],
 )
-bb2 = stk.BuildingBlock(
-    smiles=(
-        'c1cc(cc(c1)C#Cc2ccc(cc2)c3cccc4ccncc34)C#Cc5ccc(cc5)'
-        'c6cccc7ccncc67'
-    ),
+bb2 = stk.BuildingBlock.init_from_file(
+    path='big_ligand.mol',
     functional_groups=[stk.SmartsFunctionalGroupFactory(
         smarts='[#6]~[#7X2]~[#6]',
         bonders=(1, ),
@@ -123,3 +121,71 @@ mch_mol, mch_result = optimizer.get_result(
 )
 cage = cage.with_position_matrix(mch_mol.get_position_matrix())
 cage.write('moc_scipy_opt.mol')
+
+# Building a cage from the examples on the stk docs.
+bb1 = stk.BuildingBlock(
+    smiles='O=CC(C=O)C=O',
+    functional_groups=[stk.AldehydeFactory()],
+)
+bb2 = stk.BuildingBlock(
+    smiles='O=CC(Cl)(C=O)C=O',
+    functional_groups=[stk.AldehydeFactory()],
+)
+bb3 = stk.BuildingBlock('NCCN', [stk.PrimaryAminoFactory()])
+bb4 = stk.BuildingBlock.init_from_file(
+    'some_complex.mol',
+    functional_groups=[stk.PrimaryAminoFactory()],
+)
+bb5 = stk.BuildingBlock('NCCCCN', [stk.PrimaryAminoFactory()])
+
+cage = stk.ConstructedMolecule(
+    topology_graph=stk.cage.FourPlusSix(
+        # building_blocks is now a dict, which maps building
+        # blocks to the id of the vertices it should be placed
+        # on. You can use ranges to specify the ids.
+        building_blocks={
+            bb1: range(2),
+            bb2: (2, 3),
+            bb3: 4,
+            bb4: 5,
+            bb5: range(6, 10),
+        },
+    ),
+)
+cage.write('poc_scipy.mol')
+stk_long_bond_ids = get_long_bond_ids(cage)
+mch_mol = mch.Molecule(
+    atoms=(
+        mch.Atom(
+            id=atom.get_id(),
+            element_string=atom.__class__.__name__,
+        ) for atom in cage.get_atoms()
+    ),
+    bonds=(
+        mch.Bond(
+            id=i,
+            atom_ids=(
+                bond.get_atom1().get_id(),
+                bond.get_atom2().get_id(),
+            )
+        ) for i, bond in enumerate(cage.get_bonds())
+    ),
+    position_matrix=cage.get_position_matrix(),
+)
+mch_mol_nci = deepcopy(mch_mol)
+
+optimizer = mch.ScipyOptimizer(
+    target_bond_length=target_bond_length,
+    num_steps=50,
+)
+subunits = mch_mol.get_subunits(
+    bond_pair_ids=stk_long_bond_ids,
+)
+# Just get final step.
+mch_mol, mch_result = optimizer.get_result(
+    mol=mch_mol,
+    bond_pair_ids=stk_long_bond_ids,
+)
+
+cage = cage.with_position_matrix(mch_mol.get_position_matrix())
+cage.write('poc_opt_scipy.mol')
